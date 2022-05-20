222338d97d28d425ffde8818ae790be0c3bc536d
nathanreinauer <nathanreinauer@gmail.com>
2022-05-19 22:36:48 -0700

    Finished movies

--- Movie/src/Movie.java
+++ Movie/src/Movie.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Movie {
 
     private String title;
@@ -18,8 +20,36 @@ public class Movie {
         rating = "PG";
     }
 
+    public static ArrayList<Movie> getPG(ArrayList<Movie> movies)
+    {
+        ArrayList<Movie> pgMovies = new ArrayList<Movie>();
+
+        for(int i = 0; i < movies.size(); i++)
+        {
+            if(movies.get(i).rating == "PG")
+            {
+                pgMovies.add(movies.get(i));
+            }
+        }
+        return pgMovies;
+    }
+
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        Movie movie1 = new Movie("Casino Royale", "Ion Productions", "PG-13");
+        Movie movie2 = new Movie("Jurassic Park", "Universal Studios", "PG-13");
+        Movie movie3 = new Movie("E.T.", "Amblin Entertainment", "PG");
+
+        ArrayList<Movie> movies = new ArrayList<Movie>();
+        movies.add(movie1);
+        movies.add(movie2);
+        movies.add(movie3);
+
+        ArrayList<Movie> pgMovies = getPG(movies);
+
+        for(Movie movie : pgMovies)
+        {
+            System.out.println(movie.title + " is PG!");
+        }
     }
 }
