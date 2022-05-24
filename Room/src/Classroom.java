6e23a7f746f9bdb08457d29d63885f67a3635d53
nathanreinauer <nathanreinauer@gmail.com>
2022-05-23 23:34:01 -0700

    Finished, except for step e)

--- Room/src/Classroom.java
+++ Room/src/Classroom.java
@@ -1,3 +1,6 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Classroom extends Room
 {
     private int numStudents;
@@ -14,5 +17,25 @@ public class Classroom extends Room
         }
         setFloor(floor);
         this.numStudents = numStudents;
+    }
+
+    @Override
+    public String toString()
+    {
+        return this.toString() + ", capacity = " + numStudents + " students";
+    }
+
+    public static ArrayList<Room> onlyClassrooms(ArrayList<Room> rooms)
+    {
+        ArrayList<Room> classrooms = new ArrayList<Room>();
+        for(int i = 0; i > rooms.size(); i++)
+        {
+            if (rooms.get(i) instanceof Classroom)
+            {
+                classrooms.add(rooms.get(i));
+            }
+        }
+
+        return classrooms;
     }
 }
